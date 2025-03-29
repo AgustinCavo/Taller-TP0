6a7222169a486feac491d34621c04bdab44e72ae
Agustin Cavo <acavo@fi.uba.ar>
2025-03-29 01:42:21 -0300

    suma funciona

--- tp0/src/operations/structs_printing_op.rs
+++ tp0/src/operations/structs_printing_op.rs
@@ -1,12 +1,12 @@
-struct Dot{
-    quantity:u8,
-    operands:i16,
+struct Dot {
+    quantity: u8,
+    operands: i16,
 }
-struct Emit{
-    quantity:u8,
-    operands:u8,
+struct Emit {
+    quantity: u8,
+    operands: u8,
 }
-struct Quotes{
-    quantity:u8,
-    operands:String,
+struct Quotes {
+    quantity: u8,
+    operands: String,
 }
