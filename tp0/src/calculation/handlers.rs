a8e5376bef19d730008bb1595089af618ca50263
Agustin Cavo <acavo@fi.uba.ar>
2025-03-29 21:13:59 -0300

    arreglo handler condicionales

--- tp0/src/calculation/handlers.rs
+++ tp0/src/calculation/handlers.rs
@@ -85,18 +85,19 @@ pub fn conditional_operations_handler(
     if let Ok(operand) = data[*i as usize].parse::<i16>() {
         last_op.add_operand(operand);
         println!("Agregué operando: {}", operand);
+        data.pop();
         if last_op.operands() == last_op.quantity() as usize {
             match last_op.name() {
-                "drop" => {
+                "=" => {
                     data.pop();
                 }
-                "dup" => {
+                ">" => {
                     if let Some(last) = data.last() {
                         data.push(last.to_owned());
                     }
                     *i += 1;
                 }
-                "over" => {
+                "<" => {
                     let over_position = (*i) as usize;
 
                     if let Some(second) = data.get(over_position) {
@@ -104,7 +105,7 @@ pub fn conditional_operations_handler(
                     }
                     *i += 1;
                 }
-                "swap" => {
+                "and" => {
                     if let (Some(a1), Some(a2)) = (data.pop(), data.pop()) {
                         data.push(a1);
                         data.push(a2);
@@ -112,7 +113,7 @@ pub fn conditional_operations_handler(
                     *i += 1;
                 }
 
-                "rot" => {
+                "or" => {
                     if let Some(a1) = data.get(0) {
                         data.push(a1.to_string());
                         data.remove(0);
